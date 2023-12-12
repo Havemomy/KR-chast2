7362076c3e285fc207499e0861006c06217758a2
Havemomy <vovan.kop04@mail.ru>
2023-12-12 08:19:19 +0700

    finish

--- KR-chast2/MainWindow.axaml.cs
+++ KR-chast2/MainWindow.axaml.cs
@@ -10,7 +10,7 @@ namespace KR_chast2;
 
 public partial class MainWindow : Window
 {
-    public string _connString = "server = localhost; database = pro1_10; port = 3306; User = user_01 password = user01pro";
+    public string _connString = "server = 10.10.1.24; database = pro1_10; port = 3306; User = user_01; password = user01pro";
     private List<Action> _actions;
     private MySqlConnection _connection;
     public MainWindow()
@@ -21,7 +21,7 @@ public partial class MainWindow : Window
 
     private void ShowTable()
     {
-        string sql = " select * from Action";
+        string sql = " select Id,Организатор,Волонтер,Собранные_средства,Дата from Акция";
         _actions = new List<Action>();
         _connection = new MySqlConnection(_connString);
         _connection.Open();
@@ -32,10 +32,10 @@ public partial class MainWindow : Window
             var currentAction = new Action()
             {
                 ActionId = reader.GetInt32(column:"Id"),
-                Organizator = reader.GetInt32(column:"Organizator"),
-                Volonter = reader.GetInt32(column:"Volonter"),
-                Cash = reader.GetString(column:"Cash"),
-                Date = reader.GetDateTime(column:"Date")
+                Organizator = reader.GetInt32(column:"Организатор"),
+                Volonter = reader.GetInt32(column:"Волонтер"),
+                Cash = reader.GetString(column:"Собранные_средства"),
+                Date = reader.GetDateTime(column:"Дата")
             };
             _actions.Add(currentAction);
         }
@@ -51,7 +51,7 @@ public partial class MainWindow : Window
             return;
         }
         _connection.Open();
-        string sql = "delete from Action where Id = @id";
+        string sql = "delete from Акция where Id = @id";
         MySqlCommand command = new MySqlCommand(sql, _connection);
         command.Parameters.AddWithValue("@Id", selectedItem.ActionId);
         command.ExecuteNonQuery();
